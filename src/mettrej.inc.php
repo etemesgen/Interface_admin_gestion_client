bf7ee767b5a41f9f5562347911e9be27858ba9c0
Edomiyas <temesgen.edomiyas@yahoo.com>
2021-05-02 21:29:17 +0200

    commenting for better understanding and detecting error PROBLEM is ID not found by PHP

--- src/mettrej.inc.php
+++ src/mettrej.inc.php
@@ -1,18 +1,23 @@
 <?php
+
+    $id_client = ":id_client";
     //traitement des données à envoyer
     try{
-        $bdd = new PDO('mysql:host=localhost;dbname=client_ligue;charset=utf8','root','');
-        $reponse = $bdd->query('SELECT * FROM `clients` WHERE 1');
+        $bdd = new PDO('mysql:host=localhost;dbname=client_ligue;charset=utf8','root',''); // Se connecter à la BDD
+        $reponse = $bdd->query('SELECT * FROM `clients` WHERE id_client = $id_client'); // Assigner la requête cherchant l'id à une variable
 
-        while($clientUpdate = $reponse->fetch()){
-            $id = $clientUpdate['id_client']; 
-        }
+        // Définir une variable pour l'id 
+    /*    while($clientUpdate = $reponse->fetch()){
+            $id_client = $clientUpdate['id_client']; 
+        }*/
+
     }
     catch(Exception $e)
     {
         die('Erreur : '.$e->getMessage());
     }
 
+    // Vérification des champs d'entrées si tout se passe bien, envoyer la requête pour modifier le client
     if(isset($_POST['nom']) || isset($_POST['prenom']) || isset($_POST['age']) || isset($_POST['email'])){
         if(!$_POST['nom'] || !$_POST['prenom'] || !$_POST['age'] || !$_POST['email']){
                 print "<p class=\"warning\">Vous avez oublié de remplir un ou plusieurs champs !</p>";
@@ -21,14 +26,15 @@
                 print "<p class=\"warning\">Vous devez saisir des caractères...</p>";
             }
             else{
-                //update
+                // La requête mettant à jour le client
                 $sql = "UPDATE clients SET 
                     nom = :nom,
                     prenom = :prenom,
                     age = :age,
                     email = :email,
-                    WHERE id_client= ".$_GET['id_client']. "";
+                    WHERE id_clients = :id_client";
 
+                // Vérification des lignes de la BDD
                 $stmt = $bdd->prepare($sql);
                 $stmt->bindParam(':nom', $_POST['nom'], PDO::PARAM_STR);
                 $stmt->bindParam(':prenom', $_POST['prenom'], PDO::PARAM_STR);
@@ -36,10 +42,9 @@
                 $stmt->bindParam(':email', $_POST['email'], PDO::PARAM_STR);
                 $stmt->execute();
                 
-                //test pour la mise à jour
+                // Test pour la mise à jour
                 $stmt ? print "<p class=\"success\">Les modifications ont bien été enregistrées !</p>" : print "<p> Une erreur est survenue </p>"; 
-                //header("Location: index.php");
-                var_dump($stmt);
+                header("Location: index.php");
             }      
         }
 ?>
