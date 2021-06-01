ec3f5ad80d1d7baa198afef7c1858337bdd24082
Edomiyas <temesgen.edomiyas@yahoo.com>
2021-06-01 19:23:56 +0200

    Blocking on undefined index error

--- src/modele/mettrej.inc.php
+++ src/modele/mettrej.inc.php
@@ -7,7 +7,7 @@
         function clientAjour($id_client, $nom, $prenom, $age, $email){
             $reponse = $this->seConnecter()->query("SELECT * FROM `clients` WHERE id_client = ". $id_client ."");
             //$donnees = $reponse->fetch();
-                if(isset($_POST['nom']) || isset($_POST['prenom']) || isset($_POST['age']) || isset($_POST['email'])){
+                if(isset($_POST['id_client']) && isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['age']) && isset($_POST['email'])){
 
                     if(!$_POST['nom'] || !$_POST['prenom'] || !$_POST['age'] || !$_POST['email']){
                         print "<p class=\"warning\">Vous avez oublié de remplir un ou plusieurs champs !</p>";
@@ -16,24 +16,44 @@
                         print "<p class=\"warning\">Vous devez saisir des caractères...</p>";
                     } 
                     else{
+                        $requete = $bdd->prepare("
+                        UPDATE clients SET
+                            nom = ?,
+                            prenom = ?,
+                            age = ?,
+                            email = ?,
+                        WHERE id_client = ?
+                        ");
+                    
+                        // on execute 
+                        $requete->execute(
+                            [
+                                $nom, 
+                                $prenom, 
+                                $age, 
+                                $email, 
+                                $id_client
+                            ]
+                        );
                         // La requête mettant à jour le client
-                        $sql = "UPDATE clients SET 
-                            nom = :nom,
-                            prenom = :prenom,
-                            age = :age,
-                            WHERE email = :email";/*,
-                            WHERE id_clients == ".$donnees['id_client']."";*/
+                      /*  $sql = "UPDATE clients SET 
+                            nom = ?,
+                            prenom = ?,
+                            age = ?,
+                            email = ?,
+                            WHERE id_clients == ?";
 
                         // Vérification des lignes de la BDD
                         $stmt = $this->seConnecter()->prepare($sql);
+                        $stmt->bindParam(':ide', $_POST['id_clients'], PDO::PARAM_INT);
                         $stmt->bindParam(':nom', $_POST['nom'], PDO::PARAM_STR);
                         $stmt->bindParam(':prenom', $_POST['prenom'], PDO::PARAM_STR);
                         $stmt->bindParam(':age', $_POST['age'], PDO::PARAM_INT);
                         $stmt->bindParam(':email', $_POST['email'], PDO::PARAM_STR);
                         $stmt->execute();
-                
+                */
                        // Test pour la mise à jour
-                        $stmt ? print "<p class=\"success\">Les modifications ont bien été enregistrées !</p>" : print "<p> Une erreur est survenue </p>"; 
+                        $requete ? print "<p class=\"success\">Les modifications ont bien été enregistrées !</p>" : print "<p> Une erreur est survenue </p>"; 
                     }
                 }
             }
--- vue/mettre_a_jour.php
+++ vue/mettre_a_jour.php
@@ -34,7 +34,9 @@
         <a href="index.php?action=supprimerClient&supprimer"><input class="supprimer" type="submit" value="Supprimer" /></a>
         <i class="fa fa-times" id="supp"></i>
         <hr id="hr6">
-        <?php $mettreAjour->clientAjour('$id_client', "?", "?", "?", "?"); ?>
+        <?php $mettreAjour->clientAjour($_POST['idClient'], $_POST['nom'], $_POST['prenom'], $_POST['age'], $_POST['email']); 
+          var_dump(isset($_POST['nom']));
+        ?>
       </form>
     </section>
   </body>
